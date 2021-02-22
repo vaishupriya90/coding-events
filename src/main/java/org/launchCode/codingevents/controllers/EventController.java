01deb1e9ecf4392cea6137a484bb8582c577daa2
vaishu <vaishupriya90@gmail.com>
2021-02-22 16:05:46 -0600

    worj=ked on with fragments and add added few styling using bootstrap

--- src/main/java/org/launchCode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchCode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchCode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +13,11 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Boot Camp");
-        events.add("Meet up");
-        events.add("Launch code");
         model.addAttribute("events",events);
         return "events/index";
     }
@@ -25,5 +26,10 @@ public class EventController {
     public String renderCreateEventForm(){
         return "events/create";
     }
-    
+    //lives at /events/create -->post method
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
 }
--- src/main/java/org/launchCode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchCode/codingevents/controllers/HomeController.java
@@ -1,13 +1,12 @@
 package org.launchCode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping
-    public String homePage(Model model){
-        return "index";
-    }
+    public String homePage() { return "index"; }
 }
+
+
