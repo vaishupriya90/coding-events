7d148ba4a7eceed90f5256ce5b6c1aedcb16fb57
vaishu <vaishupriya90@gmail.com>
2021-02-24 20:02:59 -0600

    added a delete route and delete handler to handle data coming from the form

--- src/main/java/org/launchCode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchCode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchCode.codingevents.data.EventData;
 import org.launchCode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("events")
