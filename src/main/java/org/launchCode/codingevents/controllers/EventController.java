851f78852cc7499f2d6fdaf9f143a7de632e8a97
vaishu <vaishupriya90@gmail.com>
2021-03-18 01:14:56 -0500

    refactored the event controller to use the eventRepository to perform crud functionalities,added timezone property in the application.properties file

--- src/main/java/org/launchCode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchCode/codingevents/controllers/EventController.java
@@ -1,22 +1,27 @@
 package org.launchCode.codingevents.controllers;
 
-import org.launchCode.codingevents.data.EventData;
+import org.launchCode.codingevents.data.EventRepository;
 import org.launchCode.codingevents.models.Event;
 import org.launchCode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    @Autowired
+    private EventRepository eventRepository;
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
@@ -36,14 +41,14 @@ public class EventController {
             model.addAttribute("title","Create Event");
             return "events/create";
         }
-        EventData.add(newEvent);
+        eventRepository.save(newEvent);
         return "redirect:";
     }
 
     @GetMapping("delete")
     public String renderDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
 
@@ -51,14 +56,14 @@ public class EventController {
     public String deleteEvent(@RequestParam(required = false) int[] eventIds) {
         if (eventIds != null) {
             for (int id : eventIds) {
-                EventData.remove(id);
+                eventRepository.deleteById(id);
             }
         }
         return "redirect:";
     }
     @GetMapping("edit/{eventId}")
     public String RenderEditEventForm(Model model,@PathVariable int eventId){
-        Event eventToBeEdited = EventData.getById(eventId);
+        Event eventToBeEdited = eventRepository.findById(eventId).get();
         model.addAttribute("event",eventToBeEdited);
         String heading = "Edit Event: "+ eventToBeEdited.getName()+"(ID:"+eventToBeEdited.getId()+")";
         model.addAttribute("title",heading);
@@ -67,16 +72,20 @@ public class EventController {
     @PutMapping("edit")
     public String editEvent(@ModelAttribute @Valid Event event,int eventId,Errors errors,Model model){
         if(errors.hasErrors()){
-            Event eventToBeEdited = EventData.getById(eventId);
+            Event eventToBeEdited = eventRepository.findById(eventId).get();
             model.addAttribute("event",eventToBeEdited);
             String heading = "Edit Event: "+ eventToBeEdited.getName()+"(ID:"+eventToBeEdited.getId()+")";
             model.addAttribute("title",heading);
             return "events/edit";
         }
-//        Event eventToBeEdited = EventData.getById(eventId);
-//        eventToBeEdited.setName(name);
-//        eventToBeEdited.setDescription(description);
-        EventData.edit(event,eventId);
+        Event eventToBeEdited = eventRepository.findById(eventId).get();
+        eventToBeEdited.setName(event.getName());
+        eventToBeEdited.setDescription(event.getDescription());
+        eventToBeEdited.setContactEmail(event.getContactEmail());
+        eventToBeEdited.setLocation(event.getLocation());
+        eventToBeEdited.setNumberOfAttendees(event.getNumberOfAttendees());
+
+        eventRepository.save(eventToBeEdited);
         return "redirect:";
     }
 }
